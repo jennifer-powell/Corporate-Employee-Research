6be544e5c3ffa227668b02420c349662673d1a31
jennifer-powell <jennyruepowell@gmail.com>
2021-01-07 19:14:27 -0600

    halfway done

--- trialrun.sql
+++ trialrun.sql
@@ -6,17 +6,22 @@ FROM Employees e
 LEFT JOIN Salaries as s on
 Employees.emp_no= Salaries.emp_no;
 
--- 2. List first name, last name, and hire date for employees who were hired in 1986.-- where statement: where hire_date= jan 1986 to Dec 1986
-SELECT first_name,last_name, hire_date(1986)
-FROM empdata 
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+-- where statement: where hire_date= jan 1986 to Dec 1986
+SELECT first_name,last_name, hire_date FROM Employees
+WHERE hire_date=  1/1/1986 to 12/31/1986
+
 -- 3. List the manager of each department with the following information: 
 --department number, department name, the manager's employee number, last name, first name.
 --two inner join manager with department on dept no 
 --joined with employees table on employee number
-SELECT dept_name  FROM Departments
-  WHERE city_id IN 
-(
-    SELECT * FROM Department_Managers
+SELECT dept_no, dept_name, emp_no, last_name, first_name as 'Managers'
+FROM Department_Managers
+INNER JOIN Department_Managers on 
+Department_Mangers.dept_no= Departments.dept_no
+INNER JOIN Department_Manager on
+Employees.emp_no = Department_Manager.emp_no
+
 
 );
 
